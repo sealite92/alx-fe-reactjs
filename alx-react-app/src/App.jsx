0f68e11f14191864b210e1f97a50c3f24932b850
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-17 14:10:20 +0100

    Added userProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
