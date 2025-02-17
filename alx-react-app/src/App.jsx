e8e1c97be2cbcdab4ef00d3018f62034681d9792
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-17 13:58:13 +0100

    Added more components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
