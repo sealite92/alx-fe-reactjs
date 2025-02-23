629e1dc6429b421ec6deba622b0bc3b14c1a4985
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-23 21:19:44 +0100

    Added the props components

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,14 +1,5 @@
-import React from "react";
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
 const UserContext = createContext();
 
-export default function User({ userData }) {
-  const [user, setUser] = useState(null); // Default value for user state
-
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {userData}
-    </UserContext.Provider>
-  );
-}
+export default UserContext;
