9fcca26e060a981ebc42b155dd44940e53268b16
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-23 22:53:09 +0100

    Initial commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,10 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   <nav style={styles.navbar}>
-      <Link to="/" >Home</Link>
-      <Link to="/about" >About</Link>
-      <Link to="/services" >Services</Link>
-      <Link to="/contact" >Contact</Link>
-    </nav>
-  );
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/services">Services</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>;
 }
