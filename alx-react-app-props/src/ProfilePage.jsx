50ee42f86993e6f00c4194686a1f92d1e63712c7
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-23 21:41:00 +0100

    Added the props components

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import UserInfo from "./UserInfo";
 
-export default function ProfilePage({ userData }) {
-  return <UserInfo userData={userData} />;
+export default function ProfilePage() {
+  return <UserInfo />;
 }
