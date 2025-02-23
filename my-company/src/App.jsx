ab6e7cc1c91222f000fedc4751ce159833890c39
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-23 22:48:18 +0100

    Initial commit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  <nav style={styles.navbar}>
+      <Link to="/" >Home</Link>
+      <Link to="/about" >About</Link>
+      <Link to="/services" >Services</Link>
+      <Link to="/contact" >Contact</Link>
+    </nav>
+  );
+}
