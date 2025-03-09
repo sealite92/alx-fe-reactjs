e2d114b9b843b718b046de691ea81a35f47907a2
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-03-09 21:56:24 +0100

    new

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { QueryClient, QueryClientProvider } from "react-query";
-import PostsComponent from "./PostsComponent";
+import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
 
