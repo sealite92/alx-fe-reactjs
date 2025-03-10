8a9a4e47720acb2d37863fe52cfc484cd1f6b11b
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-03-10 22:58:35 +0100

    Added some tailwind styling

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,16 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
 
-//  module.exports = {
-//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-//   darkMode: false,
-//   theme: { extend: {} },
-//   variants: { extend: {} },
-//   plugins: [],
-// };
+export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const darkMode = false;
+export const theme = { extend: {} };
+export const variants = { extend: {} };
+export const plugins = [];
