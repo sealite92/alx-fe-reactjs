a7057433ef396153210317942c35c6938a4868de
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-03-10 22:38:15 +0100

    First commit

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+//  module.exports = {
+//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+//   darkMode: false,
+//   theme: { extend: {} },
+//   variants: { extend: {} },
+//   plugins: [],
+// };
