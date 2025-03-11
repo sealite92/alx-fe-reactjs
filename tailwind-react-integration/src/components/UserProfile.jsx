e4b39ef9982bcb2807d99d045f9d540c7de05328
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-03-11 10:50:16 +0100

    Added some responsive designs

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-xl text-blue-800 my-4 md:text-lg sm:text-xl">
+        John Doe
+      </h1>
+      <p className="text-gray-600 text-base sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
