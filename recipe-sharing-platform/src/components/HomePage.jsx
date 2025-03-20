ba95ff7b6776b8c66748225599d536b8a399b3e4
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-03-20 12:59:47 +0100

    added recipe detail with all the styling and react router

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import data from "./data.json";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   const [recipe, setRecipe] = useState(data);
@@ -8,10 +9,17 @@ export default function HomePage() {
     <>
       <div className="rounded-md shadow-sm bg-green-200 hover:bg-green-400 w-7/12 md:w-3/12 sm:grid grid-cols-2 md:grid-cols-3 sm:grid-cols-1">
         {data.map((recipe) => {
-          <div style={{ backgroundImage: `url(${recipe.image})` }}>
-            <h1 className="text-4xl">{recipe.title}</h1>
-            <p>{recipe.summary}</p>
-          </div>;
+          <>
+            <Link
+              to={`/recipe/${recipe.id}`}
+              key={recipe.id}
+              className="block bg-white shadow-md rounded-lg overflow-hidden"
+            ></Link>
+            <div style={{ backgroundImage: `url(${recipe.image})` }}>
+              <h1 className="text-4xl">{recipe.title}</h1>
+              <p>{recipe.summary}</p>
+            </div>
+          </>;
         })}
       </div>
     </>
