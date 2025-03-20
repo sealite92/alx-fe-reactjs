f98d51201eb485e59e72a9c138cef7882651b3ad
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-03-20 12:23:10 +0100

    First commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,20 +1,11 @@
 import HomePage from "./components/HomePage";
-import data from "./data.json";
+
 import "./index.css";
 
 function App() {
   return (
     <>
-      <div className="bg-red-800">
-        {data.map((recipe) => (
-          <HomePage
-            key={recipe.id}
-            title={recipe.title}
-            summary={recipe.summary}
-            image={recipe.image}
-          />
-        ))}
-      </div>
+      <HomePage />
     </>
   );
 }
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,14 +1,17 @@
-export default function HomePage({ title, summary, image }) {
+import { useEffect, useState } from "react";
+import data from "./data.json";
+
+export default function HomePage() {
+  const [recipe, setRecipe] = useState(data);
+
   return (
-    <div className="flex bg-red-400 w-full mb-5 shadow-sm rounded-lg overflow-hidden">
-      <div
-        className="w-5/12 bg-cover bg-center"
-        style={{ backgroundImage: `url(${image})` }}
-      ></div>
-      <div className="w-7/12 p-4">
-        <h1 className="text-red-700 font-bold text-xl">{title}</h1>
-        <p className="text-gray-800">{summary}</p>
+    <>
+      <div>
+        <div style={{ backgroundImage: `url(${recipe.image})` }}>
+          <h1>{recipe.title}</h1>
+          <p>{recipe.summary}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
