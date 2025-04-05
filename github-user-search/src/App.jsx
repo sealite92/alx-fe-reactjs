80aa08a8b183cae6b3c92dfabc2ae86efdd890ba
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-04-05 06:47:47 +0200

    Fixed the components

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import GitHubHome from "./components/gitHubHome";
 
 function App() {
   return (
     <div className="app">
       <h1>GitHub User Search</h1>
-      <p>Start searching for GitHub users...</p>
+      <GitHubHome />
     </div>
   );
 }
---
+++ github-user-search/src/components/GitHubHome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GitHubHome() {
+  return (
+    <div>
+      <h1>Search Github</h1>
+      <input type="text" /> <button>Search</button>
+    </div>
+  );
+}
